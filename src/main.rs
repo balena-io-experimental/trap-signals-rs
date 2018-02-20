57f1309f577c88bc39ae3eca5ef5e6755e8207cd
majorz <zahari@resin.io>
2018-02-20 12:58:07 +0200

    Not handling signals

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,101 @@
+extern crate nix;
+
+use std::thread;
+use std::time;
+use std::sync::mpsc::{channel, Sender};
+
+use nix::sys::signal::{SigSet, SIGINT, SIGQUIT, SIGTERM, SIGHUP};
+
+#[derive(Debug)]
+enum Event {
+    Working(usize),
+    Completed,
+    Terminate,
+}
+
+fn handle(exit_tx: &Sender<&'static str>) {
+    let (event_tx, event_rx) = channel();
+    let event_tx_clone = event_tx.clone();
+    
+    thread::spawn(move || {
+        process(&event_tx_clone);
+    });
+
+    thread::spawn(move || {
+        trap(&event_tx);
+    });
+
+    loop {
+        match event_rx.recv() {
+            Ok(event) => {
+                match event {
+                    Event::Working(count) => println!("Working: {}", count),
+                    Event::Completed => {
+                        exit_tx.send("Completed").unwrap();
+                        return;
+                    },
+                    Event::Terminate => {
+                        exit_tx.send("Terminated").unwrap();
+                        return;
+                    },
+                }
+            },
+            Err(e) => {
+                panic!("Error: {:?}", e);
+            },
+        }
+        
+    }
+}
+
+fn process(event_tx: &Sender<Event>) {
+    let mut count = 0;
+
+    loop {
+        count += 1;
+        event_tx.send(Event::Working(count)).unwrap();
+        thread::sleep(time::Duration::from_secs(1));
+
+        if count == 10 {
+            event_tx.send(Event::Completed).unwrap();
+            return;
+        }
+    }
+}
+
+fn trap(event_tx: &Sender<Event>) {
+    trap_signals();
+
+    event_tx.send(Event::Terminate).unwrap();
+}
+
+fn trap_signals() {
+    let mut mask = SigSet::empty();
+
+    mask.add(SIGINT);
+    mask.add(SIGQUIT);
+    mask.add(SIGTERM);
+    mask.add(SIGHUP);
+    mask.thread_block().unwrap();
+
+    let sig = mask.wait().unwrap();
+    
+    println!("Received SIG {:?}", sig);    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (exit_tx, exit_rx) = channel();
+
+    thread::spawn(move || {
+        handle(&exit_tx);
+    });
+
+    match exit_rx.recv() {
+        Ok(result) => {
+            println!("{}", result);
+        },
+        Err(e) => {
+            panic!("Error: {:?}", e);
+        },
+    }
 }
